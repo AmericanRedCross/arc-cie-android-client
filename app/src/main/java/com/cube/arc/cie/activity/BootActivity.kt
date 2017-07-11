93673b90d5b6f0afceba4df7f6cbe56b61b7846b
Callum Taylor <callum@3sidedcube.com>
2017-07-11 15:51:34 +0100

    Added entry point and empty application singleton

---
+++ app/src/main/java/com/cube/arc/cie/activity/BootActivity.kt
@@ -0,0 +1,12 @@
+package com.cube.arc.cie.activity
+
+import android.app.Activity
+
+/**
+ * Entry point of the app. This class will decide to show the onboarding feature or to progress to the
+ * main application. This class will also do a check for content updates.
+ */
+class BootActivity : Activity()
+{
+
+}
