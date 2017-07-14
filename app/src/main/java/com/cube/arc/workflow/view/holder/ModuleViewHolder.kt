a8583f8e44779b24eab9476ee39d01242ec8ee76
Callum Taylor <callum@3sidedcube.com>
2017-07-14 09:42:06 +0100

    Created module view holder class

---
+++ app/src/main/java/com/cube/arc/workflow/view/holder/ModuleViewHolder.kt
@@ -0,0 +1,16 @@
+package com.cube.arc.workflow.view.holder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.cube.arc.workflow.model.Module
+
+/**
+ * View holder for module in WorkFlowFragment recycler view
+ */
+class ModuleViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+{
+	fun populate(model : Module)
+	{
+
+	}
+}
