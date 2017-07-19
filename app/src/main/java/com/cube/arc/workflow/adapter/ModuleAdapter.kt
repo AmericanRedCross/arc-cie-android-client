47be151a71e83fe8f10aeade9f31e057563dda93
Callum Taylor <callum@3sidedcube.com>
2017-07-19 15:57:55 +0100

    Simplified module adapter

--- app/src/main/java/com/cube/arc/workflow/adapter/ModuleAdapter.kt
+++ app/src/main/java/com/cube/arc/workflow/adapter/ModuleAdapter.kt
@@ -1,11 +1,11 @@
 package com.cube.arc.workflow.adapter
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.cube.arc.R
 import com.cube.arc.workflow.model.Module
 import com.cube.arc.workflow.view.holder.ModuleViewHolder
+import com.cube.lib.util.inflate
 
 /**
  * Dataset adapter used for rendering modules in a recycler view
@@ -19,13 +19,6 @@ class ModuleAdapter : RecyclerView.Adapter<ModuleViewHolder>()
 		holder?.populate(items[position])
 	}
 
-	override fun getItemCount(): Int
-	{
-		return items.size
-	}
-
-	override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ModuleViewHolder
-	{
-		return ModuleViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.module_item_view, parent, false))
-	}
+	override fun getItemCount(): Int = items.size
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModuleViewHolder = ModuleViewHolder(parent.inflate(R.layout.module_item_view))
 }
