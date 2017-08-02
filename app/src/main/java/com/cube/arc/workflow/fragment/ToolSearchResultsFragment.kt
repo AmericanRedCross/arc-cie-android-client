c90f51fb2fad91e2a6ff01a5258bd8b70e7812f0
Callum Taylor <callum@3sidedcube.com>
2017-08-02 10:22:51 +0100

    Added stub results fragment

---
+++ app/src/main/java/com/cube/arc/workflow/fragment/ToolSearchResultsFragment.kt
@@ -0,0 +1,95 @@
+package com.cube.arc.workflow.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import com.cube.arc.R
+import com.cube.arc.workflow.adapter.ToolsAdapter
+import com.cube.arc.workflow.manager.ModulesManager
+import com.cube.arc.workflow.manager.SearchManager
+import com.cube.arc.workflow.manager.parent
+import com.cube.arc.workflow.model.Module
+import com.cube.lib.util.bind
+import com.cube.lib.util.inflate
+
+/**
+ * // TODO: Add class description
+ */
+class ToolSearchResultsFragment : Fragment()
+{
+	private val searchInput by bind<EditText>(R.id.search_input)
+	private val resultCount by bind<TextView>(R.id.result_count)
+	private val recyclerView by bind<RecyclerView>(R.id.recycler_view)
+	private val adapter = ToolsAdapter()
+	private var query: String = ""
+
+	companion object
+	{
+		fun newInstance(query: String): ToolSearchResultsFragment
+		{
+			return ToolSearchResultsFragment().apply {
+				this.query = query
+			}
+		}
+	}
+
+	override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? = container?.inflate(R.layout.tool_search_results_fragment_view)
+
+	override fun onActivityCreated(savedInstanceState: Bundle?)
+	{
+		super.onActivityCreated(savedInstanceState)
+
+		searchInput.setText(query)
+		searchInput.setOnEditorActionListener { view, actionId, event ->
+			if (actionId == EditorInfo.IME_ACTION_SEARCH)
+			{
+				search(view.text.toString())
+				true
+			}
+
+			false
+		}
+
+		ViewCompat.setNestedScrollingEnabled(recyclerView, false);
+		recyclerView.layoutManager = LinearLayoutManager(activity)
+		recyclerView.adapter = adapter
+		search(query)
+	}
+
+	fun search(searchQuery: String)
+	{
+		this.query = searchQuery
+		val searchResults = SearchManager.search(searchQuery).filter { !it.isAttachment }
+		val adapterItems = LinkedHashSet<Module>()
+		val groupHeaders = LinkedHashSet<String>()
+
+		searchResults.forEach { searchResult ->
+			val module = ModulesManager.module(searchResult.moduleId)
+
+			module?.apply {
+				var parent = parent()
+
+				parent?.let { item ->
+					groupHeaders.add(item.id)
+					adapterItems.add(item)
+				}
+
+				adapterItems.add(this)
+			}
+		}
+
+		adapter.items = adapterItems.toList()
+		adapter.groups = groupHeaders.toList()
+		adapter.notifyDataSetChanged()
+
+		resultCount.text = resources.getString(R.string.tool_search_results_count, (adapter.items.size - adapter.groups.size).toString(), searchQuery)
+	}
+}
