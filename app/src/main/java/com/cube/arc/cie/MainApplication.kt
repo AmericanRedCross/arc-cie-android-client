e276b4d829834ffe0beaa8c0b5af1fca5b76ca82
Callum Taylor <callum@3sidedcube.com>
2017-08-03 15:50:29 +0100

    Added export path constant

--- app/src/main/java/com/cube/arc/cie/MainApplication.kt
+++ app/src/main/java/com/cube/arc/cie/MainApplication.kt
@@ -2,17 +2,24 @@ package com.cube.arc.cie
 
 import android.app.Application
 import com.cube.arc.workflow.manager.ModulesManager
+import java.io.File
 
 /**
  * Application singleton for instantiating application configuration and data files
  */
 class MainApplication : Application()
 {
+	companion object
+	{
+		public lateinit var BASE_PATH: File
+	}
+
 	override fun onCreate()
 	{
 		super.onCreate()
 
 		// initialise module manager
 		ModulesManager.init(resources.assets.open("modules.json"))
+		BASE_PATH = File(getExternalFilesDir(null), "CIE-Documents")
 	}
 }
