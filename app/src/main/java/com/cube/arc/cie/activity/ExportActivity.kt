a3fd6904ccd7c92de77d3e8288be32864c36bd95
Callum Taylor <callum@3sidedcube.com>
2017-08-15 10:13:46 +0100

    Added basic downloader for critical path tools

--- app/src/main/java/com/cube/arc/cie/activity/ExportActivity.kt
+++ app/src/main/java/com/cube/arc/cie/activity/ExportActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.support.annotation.StringRes
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import com.cube.arc.R
+import com.cube.arc.cie.fragment.DownloadHelper
+import com.cube.arc.workflow.model.FileDescriptor
 import com.cube.lib.util.bind
 import com.cube.lib.util.inflate
 import kotlinx.android.synthetic.main.document_viewer_activity_view.*
@@ -19,16 +18,13 @@ import kotlinx.android.synthetic.main.document_viewer_activity_view.*
 class ExportActivity : AppCompatActivity()
 {
 	private val exportablesContainer by bind<LinearLayout>(R.id.exportables_container)
-//	private var downloadTask: DownloadHelper = DownloadHelper()
+	private var downloadTasks: HashMap<String, DownloadHelper> = hashMapOf()
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 
 		setContentView(R.layout.export_activity_view)
-
-//		downloadTask = downloadTask.attach(this)
-
 		setUi()
 	}
 
@@ -37,61 +33,91 @@ class ExportActivity : AppCompatActivity()
 		// critical path tools
 		inflateExportable(
 			exportTitle = R.string.export_critical_title,
-			exportClick = { export, progress ->
-
+			exportTask = downloadTasks.getOrPut("critical", {
+				DownloadHelper.newInstance(this@ExportActivity, "critical")
+			}).apply {
+				file = FileDescriptor(
+					title = "critital-tools",
+					url = "https://httpbin.org/bytes/${1024 * 1024 * 4}",
+					size = 1024 * 1024 * 4
+				)
 			}
 		)
 
-		// entire toolkit
-		inflateExportable(
-			exportTitle = R.string.export_toolkit_title,
-			exportClick = { export, progress ->
-
-			}
-		)
-
-		// your progress
-		inflateExportable(
-			exportTitle = R.string.export_progress_title,
-			exportClick = { export, progress ->
-
-			}
-		)
-
-		// your notes
-		inflateExportable(
-			exportTitle = R.string.export_notes_title,
-			exportClick = { export, progress ->
-
-			}
-		)
+//		// entire toolkit
+//		inflateExportable(
+//			exportTitle = R.string.export_toolkit_title,
+//			exportClick = { export, progress ->
+//
+//			}
+//		)
+//
+//		// your progress
+//		inflateExportable(
+//			exportTitle = R.string.export_progress_title,
+//			exportClick = { export, progress ->
+//
+//			}
+//		)
+//
+//		// your notes
+//		inflateExportable(
+//			exportTitle = R.string.export_notes_title,
+//			exportClick = { export, progress ->
+//
+//			}
+//		)
 
 		close.setOnClickListener {
 			finish()
 		}
 	}
 
-	private fun inflateExportable(@StringRes exportTitle: Int, exportClick: (View, ProgressBar) -> Unit): View
+	private fun inflateExportable(@StringRes exportTitle: Int, exportTask: DownloadHelper)
 	{
 		val view = exportablesContainer.inflate<View>(R.layout.exportable_view_stub).apply {
 			val title = findViewById(R.id.document_title) as TextView
 			val size = findViewById(R.id.document_size) as TextView
-			val progress = findViewById(R.id.download_progress) as ProgressBar
+			val progressBar = findViewById(R.id.download_progress) as ProgressBar
 			val export = findViewById(R.id.export) as Button
+
+			if (exportTask.isDownloading.get())
+			{
+				progressBar.visibility = View.VISIBLE
+				export.isEnabled = false
+			}
+
+			exportTask.progressLambda = { progress ->
+				progressBar.visibility = View.VISIBLE
+				progressBar.progress = progress
+			}
+
+			exportTask.callbackLambda = { success, file ->
+				Toast.makeText(this@ExportActivity, "File exported to ${file.absolutePath}", Toast.LENGTH_SHORT).show()
+
+				progressBar.visibility = View.GONE
+				progressBar.progress = 0
+				export.isEnabled = true
+			}
 
 			title.setText(exportTitle)
 			export.setOnClickListener { v ->
-				exportClick.invoke(v, progress)
+				exportTask.execute()
+
+				progressBar.visibility = View.VISIBLE
+				export.isEnabled = false
 			}
 		}
 
 		exportablesContainer.addView(view)
-		return view
 	}
 
 	override fun finish()
 	{
+		downloadTasks.values.forEach { value ->
+			value.detach()
+		}
+
 		super.finish()
-//		downloadTask.detach()
 	}
 }
