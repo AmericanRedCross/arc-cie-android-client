b1f94790a08ff8b9dc5fbb7373c4ac6763d99d57
Callum Taylor <callum@3sidedcube.com>
2017-08-17 11:35:22 +0100

    Added tinting method extensions

--- app/src/main/java/com/cube/lib/util/ViewExt.kt
+++ app/src/main/java/com/cube/lib/util/ViewExt.kt
@@ -1,6 +1,13 @@
 package com.cube.lib.util
 
 import android.app.Activity
+import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
 import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
@@ -8,6 +15,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 /**
  * Extension functions for View class
@@ -46,10 +56,55 @@ fun <T : View> bind(@IdRes idRes: Int, parent : View): Lazy<T>
 /**
  * Convenience method for inflating a view into another. Will return the inflated view, or the parent view if attach = true
  */
-fun <T : View> View.inflate(@LayoutRes layoutRes: Int, attach: Boolean = false) : T
+fun <T : View> View.inflate(@LayoutRes layoutRes: Int, attach: Boolean = false): T
 {
 	@Suppress("UNCHECKED_CAST")
 	return LayoutInflater.from(context).inflate(layoutRes, this as ViewGroup, attach) as T
 }
 
 private fun <T> unsafeLazy(initializer: () -> T) = lazy(LazyThreadSafetyMode.NONE, initializer)
+
+/**
+ * Tints views based on view type
+ */
+fun <T : View> T.tint(@ColorRes colourRes: Int): T
+{
+	val tintColour = resources.getColor(colourRes)
+	when (this)
+	{
+		is ProgressBar ->
+		{
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1)
+			{
+				indeterminateTintList = ColorStateList.valueOf(tintColour)
+				progressTintList = ColorStateList.valueOf(tintColour)
+				secondaryProgressTintList = ColorStateList.valueOf(tintColour)
+			}
+			else
+			{
+				progressDrawable.tint(tintColour)
+				indeterminateDrawable.tint(tintColour)
+			}
+		}
+
+		is ImageView -> drawable.tint(tintColour)
+		is TextView -> compoundDrawables?.tint(tintColour)
+		else -> background.tint(tintColour)
+	}
+
+	return this
+}
+
+/**
+ * Tints ann array of drawables
+ */
+fun Array<Drawable?>.tint(@ColorInt tintColour: Int) = forEach { drawable -> drawable?.tint(tintColour) }
+
+/**
+ * Tints a drawable
+ */
+fun Drawable.tint(@ColorInt tintColour: Int)
+{
+	mutate()
+	colorFilter = PorterDuffColorFilter(tintColour, PorterDuff.Mode.SRC_IN)
+}
